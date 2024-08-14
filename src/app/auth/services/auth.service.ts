580497906bb17a9a0cf4b3ac2bf6304f3550d37c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-14 08:19:08 -0700

    Logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -46,16 +46,25 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if (!token) return of(false);
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
     return this.http.get<CheckTokenResponse>(url, { headers }).pipe(
       map(({ user, token }) => this.setAuthentication(user, token)),
       catchError(() => {
-        this._authStatus.set(AuthStatus.notAuthenticaded);
+        this.logout();
         return of(false);
       })
     );
+  }
+
+  logout() {
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticaded);
+    localStorage.removeItem('token');
   }
 }
