a91efa26d99262712cd7ea18a063577c86f12af2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-14 08:01:15 -0700

    Reaccionar al cambio de autenticación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
+import { AuthStatus } from './auth/enums/auth-status.enum';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'nest-frontend';
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public finishedAuthCheck = computed<boolean>(() => {
+    if (this.authService.authStatus() === AuthStatus.checking) return false;
+
+    return true;
+  });
+
+  public authStatusChangeEffect = effect(() => {
+    switch (this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
+      case AuthStatus.authenticated:
+        return this.router.navigateByUrl('/dashboard');
+      case AuthStatus.notAuthenticaded:
+        return this.router.navigateByUrl('/auth/login');
+    }
+  });
 }
--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -9,8 +9,8 @@ export const isAuthenticatedGuard: CanActivateFn = (route, state) => {
 
   if (authService.authStatus() === AuthStatus.authenticated) return true;
 
-  // const url = state.url;
-  // localStorage.setItem('url', url);
+  if (authService.authStatus() === AuthStatus.checking) return false;
+
   router.navigateByUrl('/auth/login');
 
   return false;
